ac91c02d9d057bebe43d71bf8c9aa130d70b0f99
scarstacksolution <techinfo@scarstacksolution.com>
2024-03-28 00:52:47 -0500

    1st development changes

---
+++ EcoEnergyLiveFunctionApp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace EcoEnergyLiveFunctionApp
+{
+    public class EcoEnergyLiveFunctionApp
+    {
+        private readonly ILogger _logger;
+
+        public EcoEnergyLiveFunctionApp(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<EcoEnergyLiveFunctionApp>();
+        }
+
+        [Function("EcoEnergyLiveFunctionApp")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Hi Scarstack Solution Inc. Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
+
