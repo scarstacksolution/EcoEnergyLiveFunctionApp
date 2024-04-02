b5ac2d53b7cefe5badd940524027011abcc9eb8f
Scarstack Solution <137973466+scarstacksolution@users.noreply.github.com>
2024-04-01 19:12:14 -0500

    Update EcoEnergyLiveFunctionApp.cs

--- EcoEnergyLiveFunctionApp.cs
+++ EcoEnergyLiveFunctionApp.cs
@@ -27,19 +27,6 @@ namespace EcoEnergyLiveFunctionApp
 
             return response;
         }
-
-          [Function("EcoEnergyCampaignSuite")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Hi Scarstack Solution Inc. Welcome to Azure Functions!");
-
-            return response;
-        }
     }
 }
 
